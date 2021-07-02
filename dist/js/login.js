66e309b44b00d5b0fe5741da2c1fccdeede7c921
Ivan <ivnditchon@gmail.com>
2021-07-03 02:34:19 +0800

    Creating events on input fields

---
+++ dist/js/login.js
@@ -0,0 +1,42 @@
+import FormControl from "./form_control.js";
+
+class Login {
+    
+    constructor(loginParam = {form: document.querySelector("form"), username: document.querySelector("#username"), password: document.querySelector("#password")}) {
+        this._loginForm = loginParam.form;
+        this._username = loginParam.username;
+        this._password = loginParam.password;
+    }
+
+    input() {
+        // Focus input field
+        this._loginForm.addEventListener("focusin", (e) => {
+            
+            if (e.target.value === "" | e.target.value === null) {
+                let formControl = e.target.parentElement;
+                let form  = new FormControl(formControl, e.target);
+                form.inputActive = "login-input-active";
+                form.formControlActive = "form-control-active";
+            }
+
+        });
+        // Focus out input field
+        this._loginForm.addEventListener("focusout", (e) => { 
+            let formControl = e.target.parentElement;
+            let form  = new FormControl(formControl, e.target);
+            
+            if (e.target.value === "" | e.target.value === null) {
+                form.inputNotActive = "login-input-active";
+                form.formControlNotActive = "form-control-active";
+            }
+            else {
+                form.formControlNotActive = "form-control-active";
+            }
+
+        });
+    }
+
+}
+
+const login = new Login();
+login.input();
