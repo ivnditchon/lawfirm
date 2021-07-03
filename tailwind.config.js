e48b7d6f8ad8856b862986ad6ffc09f3b58801a2
= <ivnditchon@gmail.com>
2021-07-03 15:09:50 +0800

    Temporarily removing purge object

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,5 @@
 module.exports = {
-  purge: {
-    enabled: true,
-    content: ['./dist/**/*.{html, js}']
-  },
+  purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
     screens: {
