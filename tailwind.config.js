7f094ddd691a216c8c72e4ed85ea3da9fc472d85
Ivan <ivnditchon@gmail.com>
2021-07-21 02:09:49 +0800

    Create purge manually

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-  purge: [],
+  purge: {
+    enabled: true,
+    content: ['./dist/**/*.{html, js}']
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     screens: {
