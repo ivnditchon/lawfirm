0d363c8c7f3ee9b2ac29432ec4de6d9e6c395820
Ivan <ivnditchon@gmail.com>
2021-07-28 22:45:56 +0800

    Creating functions for dropdown cases and payments

--- dist/js/admin.js
+++ dist/js/admin.js
@@ -1,9 +1,9 @@
-// Object literals
+// Using Object literals
 const admin = {
 
-    // DOM elemenst
+    // DOM elements
     elem: {
-        menu: document.querySelector("#menu"),
+        sidebarMenu: document.querySelector("#menu"),
         sidebar: document.querySelector("#sidebar"),
         sidebarHeader: document.querySelector("#sidebar-header"),
         sidebarLogo: document.querySelector("#sidebar-logo"),
@@ -11,55 +11,60 @@ const admin = {
         navDropdownIcon: document.querySelectorAll("#dropdown-icon"),
         navCases: document.querySelector("#cases"),
         navPayments: document.querySelector("#payments"),
-        overlay: document.querySelector("#overlay")
+        overlay: document.querySelector("#overlay"),
     },
 
     // Menu toggle
-    menuToggle: () => {
-        admin.elem["menu"].addEventListener("click", () => {
-            admin.sidebarActive();
-            admin.sidebarHeaderActive();
-            admin.navLinkActive();
-            admin.overlayActive();
-
-            if (Array.from(admin.elem["navLink"], linkName => linkName.classList.contains("admin-link-name-active"))) {
-                Array.from(admin.elem["navDropdownIcon"], icon => icon.addEventListener("click", e => {
-                    
-                    if (e.target.classList.contains("cases")) {
-                        admin.elem["navCases"].classList.toggle("admin-dropdown-active");
-                    }
-
-                    if (e.target.classList.contains("payments")) {
-                        admin.elem["navPayments"].classList.toggle("admin-dropdown-active");
-                    }
-
-                }));
-            } 
-
+    menuToggle() {
+        admin.elem["sidebarMenu"].addEventListener("click", e => {
+            e.preventDefault();
+            admin.sidebar();
         });
     },
 
-    // Sidebar active
-    sidebarActive: () => {
+    // Sidebar 
+    sidebar() {
         admin.elem["sidebar"].classList.toggle("admin-sidebar-active");
-    },
-
-    // Sidebar header active
-    sidebarHeaderActive: () => {
         admin.elem["sidebarHeader"].classList.toggle("admin-sidebar-header-active");
         admin.elem["sidebarLogo"].classList.toggle("admin-sidebar-logo-active");
-    },
+        admin.elem["overlay"].classList.toggle("admin-overlay-active");
+        Array.from(admin.elem["navLink"], linkName => linkName.classList.toggle("admin-link-name-active"));
+        Array.from(admin.elem["navDropdownIcon"], icon => {
+            icon.classList.toggle("admin-dropdown-icon-active");
+            icon.addEventListener("click", e => {
+                e.preventDefault();
+                
+                if (e.target.classList.contains("cases")) {
+                    e.target.classList.toggle("admin-nav-icon-rotate");
+                    admin.elem["navCases"].classList.toggle("admin-dropdown-active");
+                }
+                
+                if (e.target.classList.contains("payments")) {
+                    e.target.classList.toggle("admin-nav-icon-rotate");
+                    admin.elem["navPayments"].classList.toggle("admin-dropdown-active");
+                }
+            });
+        });
 
-    // Navlink active
-    navLinkActive: () => {
-        Array.from(admin.elem["navLink"], (linkName) => linkName.classList.toggle("admin-link-name-active"));
-    },
+        if (!admin.elem["sidebar"].classList.contains("admin-sidebar-active")) {
+            Array.from(admin.elem["navDropdownIcon"], icon => {
+                icon.classList.remove("admin-dropdown-icon-active");
+                
+                if (icon.classList.contains("cases")) {
+                    icon.classList.remove("admin-nav-icon-rotate");
+                    admin.elem["navCases"].classList.remove("admin-dropdown-active");
+                }
 
-    // Overlay active
-    overlayActive: () => admin.elem["overlay"].classList.toggle("admin-overlay-active"),
+                if (icon.classList.contains("payments")) {
+                    icon.classList.remove("admin-nav-icon-rotate");
+                    admin.elem["navPayments"].classList.remove("admin-dropdown-active");
+                }
+            });
+        }
+    },
 
     // Initializer
-    init: () => admin.menuToggle()
+    init() { admin.menuToggle(); }
 
 };
 // Destructuring object
