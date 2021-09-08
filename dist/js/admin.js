2a579b911c637e900343f13aac0954701a7ef24a
Ivan <ivnditchon@gmail.com>
2021-09-08 23:04:19 +0800

    Creating function for sidebar width if the screen is equal or greater than 1280

--- dist/js/admin.js
+++ dist/js/admin.js
@@ -29,41 +29,82 @@ const admin = {
     // Sidebar 
     sidebar() {
         admin.elem["body"].classList.toggle("body");
-        admin.elem["sidebar"].classList.toggle("admin-sidebar-active");
-        admin.elem["sidebarHeader"].classList.toggle("admin-sidebar-header-active");
-        admin.elem["sidebarLogo"].classList.toggle("admin-sidebar-logo-active");
-        admin.elem["overlay"].classList.toggle("admin-overlay-active");
-        Array.from(admin.elem["navLink"], linkName => linkName.classList.toggle("admin-link-name-active"));
-        Array.from(admin.elem["navDropdownIcon"], icon => {
-            icon.classList.toggle("admin-dropdown-icon-active");
-            icon.addEventListener("click", e => {
-               
-                if (e.target.classList.contains("cases")) {
-                    e.target.classList.toggle("admin-nav-icon-rotate");
-                    admin.elem["navCases"].classList.toggle("admin-dropdown-active");
-                }
-                
-                if (e.target.classList.contains("payments")) {
-                    e.target.classList.toggle("admin-nav-icon-rotate");
-                    admin.elem["navPayments"].classList.toggle("admin-dropdown-active");
-                }
-            });
-        });
-
-        if (!admin.elem["sidebar"].classList.contains("admin-sidebar-active")) {
+        
+        if (screen.width >= 1280) {
+            admin.elem["sidebar"].classList.toggle("w-1/5");
+            admin.elem["sidebarHeader"].classList.toggle("admin-sidebar-header-active");
+            admin.elem["sidebarLogo"].classList.toggle("admin-sidebar-logo-active");
+            admin.elem["overlay"].classList.toggle("admin-overlay-active");
+            Array.from(admin.elem["navLink"], linkName => linkName.classList.toggle("admin-link-name-active"));
             Array.from(admin.elem["navDropdownIcon"], icon => {
-                icon.classList.remove("admin-dropdown-icon-active");
+                icon.classList.toggle("admin-dropdown-icon-active");
+                icon.addEventListener("click", e => {
                 
-                if (icon.classList.contains("cases")) {
-                    icon.classList.remove("admin-nav-icon-rotate");
-                    admin.elem["navCases"].classList.remove("admin-dropdown-active");
-                }
-
-                if (icon.classList.contains("payments")) {
-                    icon.classList.remove("admin-nav-icon-rotate");
-                    admin.elem["navPayments"].classList.remove("admin-dropdown-active");
-                }
+                    if (e.target.classList.contains("cases")) {
+                        e.target.classList.toggle("admin-nav-icon-rotate");
+                        admin.elem["navCases"].classList.toggle("admin-dropdown-active");
+                    }
+                    
+                    if (e.target.classList.contains("payments")) {
+                        e.target.classList.toggle("admin-nav-icon-rotate");
+                        admin.elem["navPayments"].classList.toggle("admin-dropdown-active");
+                    }
+                });
             });
+
+            if (!admin.elem["sidebar"].classList.contains("w-1/5")) {
+                Array.from(admin.elem["navDropdownIcon"], icon => {
+                    icon.classList.remove("admin-dropdown-icon-active");
+                    
+                    if (icon.classList.contains("cases")) {
+                        icon.classList.remove("admin-nav-icon-rotate");
+                        admin.elem["navCases"].classList.remove("admin-dropdown-active");
+                    }
+
+                    if (icon.classList.contains("payments")) {
+                        icon.classList.remove("admin-nav-icon-rotate");
+                        admin.elem["navPayments"].classList.remove("admin-dropdown-active");
+                    }
+                });
+            }
+        }
+        else {
+            admin.elem["sidebar"].classList.toggle("admin-sidebar-active");
+            admin.elem["sidebarHeader"].classList.toggle("admin-sidebar-header-active");
+            admin.elem["sidebarLogo"].classList.toggle("admin-sidebar-logo-active");
+            admin.elem["overlay"].classList.toggle("admin-overlay-active");
+            Array.from(admin.elem["navLink"], linkName => linkName.classList.toggle("admin-link-name-active"));
+            Array.from(admin.elem["navDropdownIcon"], icon => {
+                icon.classList.toggle("admin-dropdown-icon-active");
+                icon.addEventListener("click", e => {
+                
+                    if (e.target.classList.contains("cases")) {
+                        e.target.classList.toggle("admin-nav-icon-rotate");
+                        admin.elem["navCases"].classList.toggle("admin-dropdown-active");
+                    }
+                    
+                    if (e.target.classList.contains("payments")) {
+                        e.target.classList.toggle("admin-nav-icon-rotate");
+                        admin.elem["navPayments"].classList.toggle("admin-dropdown-active");
+                    }
+                });
+            });
+
+            if (!admin.elem["sidebar"].classList.contains("admin-sidebar-active")) {
+                Array.from(admin.elem["navDropdownIcon"], icon => {
+                    icon.classList.remove("admin-dropdown-icon-active");
+                    
+                    if (icon.classList.contains("cases")) {
+                        icon.classList.remove("admin-nav-icon-rotate");
+                        admin.elem["navCases"].classList.remove("admin-dropdown-active");
+                    }
+
+                    if (icon.classList.contains("payments")) {
+                        icon.classList.remove("admin-nav-icon-rotate");
+                        admin.elem["navPayments"].classList.remove("admin-dropdown-active");
+                    }
+                });
+            }
         }
     },
 
